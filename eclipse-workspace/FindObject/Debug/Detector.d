20b773258e7aaab78c1f16072ec6b420eb0055d7
Cody-41 <din.madshus@gmail.com>
2017-07-21 00:12:48 +0300

    commit

---
+++ eclipse-workspace/FindObject/Debug/Detector.d
@@ -0,0 +1,4 @@
+Detector.o: ../Detector.cpp \
+ /home/regardunix/Downloads/opencv/include/opencv2/opencv.hpp
+
+/home/regardunix/Downloads/opencv/include/opencv2/opencv.hpp:
---
+++ eclipse-workspace/FindObject/Debug/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../Detector.cpp 
+
+OBJS += \
+./Detector.o 
+
+CPP_DEPS += \
+./Detector.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -I/home/regardunix/Downloads/opencv/include -I/home/regardunix/Downloads/opencv/include/opencv -I/home/regardunix/Downloads/opencv/include/opencv2 -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
